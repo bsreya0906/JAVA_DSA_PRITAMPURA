24e56b7e3a23b8d6cb4b3dd7afe6345dbf292471
Student ApnaCollege <bsreya0906@gmail.com>
2024-07-13 20:45:45 +0530

    first commit of day1

--- flowchart.java
+++ flowchart.java
@@ -8,9 +8,33 @@ public class flowchart{
         System.out.println("  without next line ");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println("value input by user is :");
+      System.out.println("value input by user is :");
         System.out.println(a);
         sc.close();
+
+        /* ~ Data-types(8) :
+        1. byte (1 byte)
+        2. char (2 byte)
+        3. long (8 byte)
+        4. double (8 byte)
+        5. float (4 byte)
+        6. int (4 byte)
+        7. short (2 byte)
+        8. boolean (1 BIT)
+        ( 1 BYTE = 8 BITS )
+
+        ~ operators (5) 
+        1. Arithematic (+,-,/,%,*,++,--)
+        2. Logical (&&, ||, !)
+        3. Comparison (>,<,==,!=,>=,<=)
+        4  Bitwise (>>(left shift ),<<(right shift), &,|,^(XOR),~(Negation))
+        5. Assignment (=, +=,-=,&=,>>=,<<=,|=,*=,/=,etc.)
+        */
+        
+          
+
+        
+
 
 
 
