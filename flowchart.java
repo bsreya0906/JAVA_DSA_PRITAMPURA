d45d1b60cebbdcda1c44494b3f0fa4bb165c4c9d
Student ApnaCollege <bsreya0906@gmail.com>
2024-07-13 12:35:07 +0530

    first commit

---
+++ flowchart.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class flowchart{
+    public static void main(String[] args) {
+        //output
+        System.out.println(" hemlooo !!");
+        System.out.print(" no next line");
+        System.out.println("  without next line ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("value input by user is :");
+        System.out.println(a);
+        sc.close();
+
+
+
+    }
+
+
+    }
